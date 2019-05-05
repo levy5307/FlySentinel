b88b3c7a246bdb8c633406cb7174c4746bb17580
levy5307 <zlw5307@163.com>
2019-05-05 23:24:50 +0800

    添加FlyInstance

---
+++ src/flyInstance/FlyInstance.cpp
@@ -0,0 +1,9 @@
+//
+// Created by levy on 2019/5/5.
+//
+
+#include "FlyInstance.h"
+
+FlyInstance::FlyInstance() {
+
+}
---
+++ src/flyInstance/FlyInstance.h
@@ -0,0 +1,16 @@
+//
+// Created by levy on 2019/5/5.
+//
+
+#ifndef FLYSENTINEL_FLYINSTANCE_H
+#define FLYSENTINEL_FLYINSTANCE_H
+
+class FlyInstance {
+public:
+    FlyInstance();
+
+private:
+};
+
+
+#endif //FLYSENTINEL_FLYINSTANCE_H
