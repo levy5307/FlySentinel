803ba562a9841fc7c4b546b78f0bafe11ddbc87e
levy5307 <zlw5307@163.com>
2019-05-09 22:40:50 +0800

    完善InstanceLink内部变量

--- src/instanceLink/InstanceLink.h
+++ src/instanceLink/InstanceLink.h
@@ -5,10 +5,22 @@
 #ifndef FLYSENTINEL_INSTANCELINK_H
 #define FLYSENTINEL_INSTANCELINK_H
 
+#include <cstdint>
 #include "../coordinator/interface/AbstractInstanceLink.h"
 
 class InstanceLink : public AbstractInstanceLink {
-
+private:
+    bool disconnected;              // true-需要重连连接
+    int pendingCommands;            // 等待响应的命令数量
+    uint64_t ccConnTime;            // cc连接时间
+    uint64_t pcConntime;            // pc连接时间
+    uint64_t pcLastActivity;        // 最后一次收到消息的时间
+    uint64_t lastAvailTime;         // 上次可用时间
+    uint64_t actPingTime;           // 距离上次接收到pong以来，最早的一次发送ping的时间。
+                                    // 如果接收到pong，该值会被设置为0。如果当前值为0并且发送了ping，则该值被设置为当前时间
+    uint64_t lastPingTime;          // 最后一次发送ping的时间
+    uint64_t lastPongTime;          // 最后一次接收pong的时间
+    uint64_t lastReconnTime;        // 最后一次进行重连的时间
 };
 
 
